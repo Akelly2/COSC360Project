364299b020863c6f52234a864dfc646214d4a5ef
Akelly2 <alex_point_zero@hotmail.com>
2017-03-06 23:29:53 -0800

    Put example links on homepage

--- header.php
+++ header.php
@@ -4,7 +4,6 @@
 ?>
 <header>
     <a class="HFlink" href="index.php">Home</a>
-    <a class="HFlink" href="create.php">Create</a>
     <a class="HFlink" href="login.php">Login</a>
     <a class="HFlink" href="register.php">Register</a>
 </header>
--- index.php
+++ index.php
@@ -10,10 +10,45 @@
     <body>
         <?php include 'header.php'; ?>
         <div id="sidebar">
-
+            <a href="create.php">Create</a>
         </div>
         <div id="links">
-
+            <div id="submission">
+                <h3>Check out this sweet pencil.</h3>
+                <p>
+                    Submitted 5 hours ago by Joey K.
+                </p>
+                <p>
+                    <a href="#">Comments</a>
+                </p>
+            </div>
+            <div id="submission">
+                <h3>Who exactly is Scott F.?</h3>
+                <p>
+                    Submitted 6 hours ago by Jeffrey D.
+                </p>
+                <p>
+                    <a href="#">Comments</a>
+                </p>
+            </div>
+            <div id="submission">
+                <h3>Let's talk politics. What could go wrong?</h3>
+                <p>
+                    Submitted 7 hours ago by John H.
+                </p>
+                <p>
+                    <a href="#">Comments</a>
+                </p>
+            </div>
+            <div id="submission">
+                <h3>Check out my really cool pencil.</h3>
+                <p>
+                    Submitted 14 hours ago by Adam S.
+                </p>
+                <p>
+                    <a href="#">Comments</a>
+                </p>
+            </div>
         </div>
 
         <?php include 'footer.php' ?>
