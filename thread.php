52094f60d76b9756d5f88bb5188fb4393423561e
Akelly2 <alex_point_zero@hotmail.com>
2017-03-28 23:07:43 -0700

    gitignore change

--- thread.php
+++ thread.php
@@ -1,3 +1,24 @@
+<?php
+include("app/DB.php");
+if (!isset($_SESSION)) {
+    session_start();
+}
+if (!isset($_GET['postid'])) {
+    header('Location: index.php');
+}
+$conn = DB::getConnection() or
+  die ("<p>Data problem. Talk to your administrator.</p>");
+$postid = $_GET['postid'];
+$sql = "SELECT postid, title, content, ts, userid, U.username
+        FROM Post as P, User as U
+        WHERE postid = ? AND U.userid = P.userid";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param('i', $postid);
+$stmt->execute();
+$result = $stmt->get_result();
+$row = $result->fetch_array(MYSQLI_NUM);
+
+ ?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -13,6 +34,14 @@
         <div id="sidebar">
             <a class="speciallink" href="create.php">Create</a>
         </div>
+
+        <div id="thepost">
+            <h4><?= $row[1] ?></h4>
+            <p>
+                
+            </p>
+        </div>
+
         <?php // Logic is needed here to show entire comment threads ?>
         <div id="links">
             <div id="comment">
